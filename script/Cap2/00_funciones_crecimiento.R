0e85fc007ca7f077a7a8e35cace33552ea09bfbb
APDataSc <sklan8@hotmail.com>
2025-03-03 13:06:28 -0600

    Built site for gh-pages

--- script/Cap2/00_funciones_crecimiento.R
+++ script/Cap2/00_funciones_crecimiento.R
@@ -10,7 +10,7 @@
 #         Creado por:               Andrés Peña M.   
 #         Fecha de creación:        27-02-2025
 #         Actualizado por:          Andrés Peña M.
-#         Fecha de actualización:   28-02-2025
+#         Fecha de actualización:   03-03-2025
 #         Institución:              Facultad de Ciencias - UNAM
 #         Contacto:                 agpena@colmex.mx
 #
@@ -25,7 +25,7 @@ graphics.off( )
 
 ## b. Instalación de paquetes  
 # Listado de paquetes 
-.packages <-  c( "data.table", "lubridate")
+.packages <-  c( "data.table", "lubridate", "latex2exp")
 # Instalacion de paquetes no instalados
 .inst <- .packages %in% installed.packages()
 if( length( .packages[ !.inst ] ) > 0 ){
@@ -47,6 +47,7 @@ data <- fread("https://raw.githubusercontent.com/APDataSc/ciencias_unam_2025-2/r
 # 1. Ejemplo de funciones ----
 
 x <- 1:10
+x <- 1:50
 
 # Equiponderado
 w <- rep(1/10, 10)
@@ -64,7 +65,49 @@ return(average)
 
 }
 
+wei_ave(x)
 wei_ave(x, w)
+
+
+## 1.1 Ejemplo de TBM ----
+
+# Tasa bruta de mortalidad = D/N
+(TBM <- data[ , .(D=sum(D), N=sum(N)), .(location)] %>% 
+      .[ , d:=D/N*1000])
+TBM
+
+# Estructura por edad (C_x) y Tasas específicas de mortalidad (m_x)
+data[ , `:=`(C=N/sum(N), m=D/N), .(location)]
+
+
+# Gráfica de estructura por edad
+plot(data[location=="Guatemala", x], 
+     data[location=="Guatemala", C], col = "red", type = "l", 
+     xlab = "Edad (x)", ylab = TeX("$_{n} C_{x}$"))
+lines(data[location=="Argentina", x], 
+      data[location=="Argentina", C], col = "blue", type = "l")
+
+# Gráfica de tasas específicas de mortalidad
+plot(data[location=="Argentina", x], 
+     data[location=="Argentina", log(m)], col = "blue", type = "l",
+     xlab = "Edad (x)", ylab = TeX("$ln(_{n} m_{x}$)"))
+lines(data[location=="Guatemala", x], 
+      data[location=="Guatemala", log(m)], col = "red", type = "l")
+
+# Tasa bruta de mortalidad como promedio ponderado por edades específicas
+(TBM_C <- data[ , .(d_c=wei_ave(m, C)*1000), .(location)])
+
+
+# Tasa bruta de mortalidad estandarizada a la estructura de Guatemala
+gua_C <- data[location=="Guatemala", C] 
+data[ , C_s := c(gua_C, gua_C)]
+
+(TBM_Cs <- data[ , .(d_cs=wei_ave(m, C_s)*1000), .(location)])
+
+
+# Unión de los tres tipos de tasa calculados
+left_join(TBM, TBM_C, by="location") %>% 
+  left_join(TBM_Cs, by="location")
 
 
 
