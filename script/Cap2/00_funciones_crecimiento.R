c8d1bfebc23219d281dc33326618bd2f617a59e0
APDataSc <sklan8@hotmail.com>
2025-03-06 09:06:31 -0600

    Script crecimiento updated!!!

--- script/Cap2/00_funciones_crecimiento.R
+++ script/Cap2/00_funciones_crecimiento.R
@@ -25,7 +25,7 @@ graphics.off( )
 
 ## b. Instalación de paquetes  
 # Listado de paquetes 
-.packages <-  c( "data.table", "lubridate", "latex2exp")
+.packages <-  c( "data.table", "lubridate", "latex2exp", "dplyr")
 # Instalacion de paquetes no instalados
 .inst <- .packages %in% installed.packages()
 if( length( .packages[ !.inst ] ) > 0 ){
@@ -106,7 +106,7 @@ data[ , C_s := c(gua_C, gua_C)]
 
 
 # Unión de los tres tipos de tasa calculados
-left_join(TBM, TBM_C, by="location") %>% 
+left_join(TBM, TBM_C, by="location") |> 
   left_join(TBM_Cs, by="location")
 
 
@@ -128,13 +128,13 @@ lin <- function(N_0, N_T, t_0, t_T, t){
 
 cre_lin <- lin( N_0=112336538, N_T=126014024, 
                 t_0="2010-06-25", t_T="2020-03-15",     
-                  t=seq(2021.5, 2100.5, 1)
+                  t=seq(2020.5, 2100.5, 1)
 )
 
 
 # Gráfica
 data.frame(Año = c(decimal_date(as.Date("2010-06-25")), 
-                   decimal_date(as.Date("2020-03-15")), seq(2021.5, 2100.5, 1)),
+                   decimal_date(as.Date("2020-03-15")), seq(2020.5, 2100.5, 1)),
            N_t   = c(112336538, 126014024, cre_lin)) %>% 
   ggplot(aes(Año, N_t)) + 
   geom_line() + 
@@ -159,11 +159,12 @@ geo <- function(N_0, N_T, t_0, t_T, t){
 
 cre_geo <- geo( N_0=112336538, N_T=126014024, 
                 t_0="2010-06-25", t_T="2020-03-15",     
-                t=seq(2021.5, 2100.5, 1)
+                t=seq(2020.5, 2100.5, 1)
 )
 
 # Gráfica
-data.frame(Año = c(2010+5/12+25/365, 2020+2/12+15/365, seq(2021.5, 2100.5, 1)),
+data.frame(Año = c(decimal_date(as.Date("2010-06-25")), 
+                   decimal_date(as.Date("2020-03-15")), seq(2020.5, 2100.5, 1)),
            N_t   = c(112336538, 126014024, cre_geo)) %>% 
   ggplot(aes(Año, N_t)) + 
   geom_line() + 
@@ -187,16 +188,59 @@ expo <- function(N_0, N_T, t_0, t_T, t){
 
 cre_exp <- expo( N_0=112336538, N_T=126014024, 
                  t_0="2010-06-25", t_T="2020-03-15",     
-                 t=seq(2021.5, 2100.5, 1)
+                 t=seq(2020.5, 2100.5, 1)
 )
 
 # Gráfica
-data.frame(Año = c(2010+5/12+25/365, 2020+2/12+15/365, seq(2021.5, 2100.5, 1)),
+data.frame(Año = c(decimal_date(as.Date("2010-06-25")), 
+                   decimal_date(as.Date("2020-03-15")), seq(2020.5, 2100.5, 1)),
            N_t   = c(112336538, 126014024, cre_exp)) %>% 
   ggplot(aes(Año, N_t)) + 
   geom_line() + 
   geom_point()
 
+
+# 4. Crecimiento logístico ----
+
+logistic <- function(U = 200000000, L = 40000000, 
+                     anio_ini=1950.5, anio_fin=2100.5,
+                     anio=decimal_date(as.Date(c("2010-06-25", "2020-03-15"))), 
+                     I=c(112336538, 126014024)){
+  U <- U
+  L <- L 
+  anio_ini <- anio_ini
+  anio_fin <- anio_fin
+  anio <- decimal_date(as.Date(anio))
+  anios <- anio_fin
+  I <- I 
+  
+  Z <- log((U-I)/(I-L))
+  a <- lm(Z~anio)$coefficients[1]
+  w <- lm(Z~anio)$coefficients[2]
+  
+  Z_t <- a+w*anios
+  I_t <- (U+L*exp(Z_t))/(1+exp(Z_t))
+  
+  logistic <- setDT(as.data.frame(cbind(anios, I_t)))
+  logistic[anios>anio[length(anio)], .(I_t)] %>% pull() 
+  
+}
+
+
+cre_log <- logistic(U = 200000000, L = 40000000, 
+           anio_ini = 1950.5, 
+           anio_fin = seq(2020.5, 2100.5, 1), 
+           anio = c("2010-06-25", "2020-03-15"), 
+           I = c(112336538, 126014024)   
+           )
+
+# Gráfica
+data.frame(Año = c(decimal_date(as.Date("2010-06-25")), 
+                   decimal_date(as.Date("2020-03-15")), seq(2020.5, 2100.5, 1)),
+           N_t   = c(112336538, 126014024, cre_log)) %>% 
+  ggplot(aes(Año, N_t)) + 
+  geom_line() + 
+  geom_point()
 
 
 # 5. Crecimiento consolidado ----
@@ -204,22 +248,61 @@ data.frame(Año = c(2010+5/12+25/365, 2020+2/12+15/365, seq(2021.5, 2100.5, 1)),
 crecim <- function(N_0, N_T, t_0, t_T, t, type = "lin"){
   
   # si se quiere el modelo exponencial
-  if(tipo == "exp"){
+  if(type == "exp"){
     Nh <- expo(N_0, N_T, t_0, t_T, t) # una función dentro de otra!!!
   }
   
   # si se quiere el modelo lineal
-  if(tipo == "lin"){
+  if(type == "lin"){
     Nh <- lin(N_0, N_T, t_0, t_T, t) # una función dentro de otra!!!
   }
   
   # si se quiere el modelo geométrico
-  if(tipo == "geo"){
+  if(type == "geo"){
     Nh <- geo(N_0, N_T, t_0, t_T, t) # una función dentro de otra!!!
+  }
+  
+  # si se quiere el modelo geométrico
+  if(type == "log"){
+    Nh <- logistic(U = 200000000, L = 40000000,
+                   anio_ini = 1950, anio_fin = t,
+                   anio = c(t_0, t_T),
+                   I = c(N_0, N_T)
+                    ) # una función dentro de otra!!!
   }
   
   return(Nh)
 }
+
+
+lin <- crecim(N_0 = 112336538, N_T = 126014024, 
+       t_0 = "2010-06-25", t_T = "2020-03-15", 
+       t = seq(2020.5, 2100.5, 1), type = "lin")
+
+exp <- crecim(N_0 = 112336538, N_T = 126014024, 
+       t_0 = "2010-06-25", t_T = "2020-03-15", 
+       t = seq(2020.5, 2100.5, 1), type = "exp")
+
+log <- crecim(N_0 = 112336538, N_T = 126014024, 
+       t_0 = "2010-06-25", t_T = "2020-03-15", 
+       t = seq(2020.5, 2100.5, 1), type = "log")
+
+
+
+data.table(Año = c(decimal_date(as.Date("2010-06-25")), 
+                   decimal_date(as.Date("2020-03-15")), seq(2020.5, 2100.5, 1)),
+           lin = c(112336538, 126014024, lin), 
+           exp = c(112336538, 126014024, exp), 
+           log = c(112336538, 126014024, log)) %>%
+           melt.data.table(id.vars = "Año", variable.name = "Crec", 
+                           value.name = "Pop") %>% 
+  ggplot(aes(x=Año, y=Pop, group=Crec, color=Crec)) +
+  geom_line() + geom_point() +
+  scale_color_brewer(palette="Paired") +
+  theme_minimal() +
+  geom_hline(yintercept = 126014024*2, linetype = 'dashed', 
+             color = 'red', 
+             size = 0.54, alpha = 0.70)
 
 
 "--------------------------------------FIN-------------------------------------"
